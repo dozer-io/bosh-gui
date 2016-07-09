d41e7569bfa5bb9e63c963c2a27aa8c14d74a2cc
Ruben Koster <rkoster@starkandwayne.com>
2016-07-09 16:51:08 +0200

    Implement GetVMsTask msg

--- src/VM.elm
+++ src/VM.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+module VM exposing (..)
 
 import Html exposing (..)
 import Html.App as App
--- src/VMs.elm
+++ src/VMs.elm
@@ -2,19 +2,11 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.App as App
-import Html.Attributes exposing (..)
 import Http
 import Json.Decode exposing (..)
-import List exposing (map)
-import Material
 import Platform.Cmd exposing (Cmd)
 import Task
-
-
--- import Task.Extra exposing (parallel)
--- import Material.Scheme
--- import Material.Button as Button
--- import Material.Options exposing (css)
+import VM
 
 
 main : Program Never
@@ -32,9 +24,10 @@ main =
 
 
 type alias Model =
-    { vms : List ( Deployment, List VM )
-    , deployments : List Deployment
-    , mdl : Material.Model
+    { deployment : Deployment
+    , vms : List VM.Model
+    , loading : Bool
+    , taskUrl : String
     }
 
 
@@ -42,13 +35,13 @@ type alias Deployment =
     String
 
 
-type alias VM =
-    { name : String }
+type alias TaskUrl =
+    String
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model [] [] Material.model, fetchDeployments )
+    ( Model "cf-warden" [] True "", getVMsTask "cf-warden" )
 
 
 
@@ -56,61 +49,39 @@ init =
 
 
 type Msg
-    = FetchVMs
-    | FetchVMsSucceed (List (List VM))
-    | FetchDeploymentsSucceed (List Deployment)
-    | FetchFail Http.Error
-    | MDL Material.Msg
+    = GetVMsTaskFail Http.Error
+    | GetVMsTaskSucceed TaskUrl
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update action model =
-    case action of
-        FetchVMs ->
-            ( model, fetchDeployments )
+update msg model =
+    case msg of
+        GetVMsTaskFail _ ->
+            ( model, getVMsTask model.deployment )
 
-        FetchDeploymentsSucceed deployments ->
-            ( { model | deployments = deployments }, fetchVMs deployments )
-
-        FetchVMsSucceed vms ->
-            --            ( { model | vms = (Dict.insert deployment vms model.vms) }, Cmd.none )
-            ( model, Cmd.none )
-
-        FetchFail _ ->
-            ( model, Cmd.none )
-
-        MDL action' ->
-            Material.update MDL action' model
+        GetVMsTaskSucceed taskUrl ->
+            ( { model | taskUrl = taskUrl }, Cmd.none )
 
 
 
 -- VIEW
 
 
-type alias Mdl =
-    Material.Model
-
-
 view : Model -> Html Msg
 view model =
     div []
-        <| List.map deploymentList model.vms
-
-
-deploymentList : ( String, List VM ) -> Html Msg
-deploymentList namedVMs =
-    div []
-        [ h3 []
-            [ text <| fst namedVMs
-            , ul [ attribute "class" "mdl-list" ]
-                (List.map vmListItem <| snd namedVMs)
-            ]
+        [ (if model.loading then
+            loading
+           else
+            text ""
+          )
+        , text model.taskUrl
         ]
 
 
-vmListItem : VM -> Html Msg
-vmListItem vm =
-    li [ attribute "class" "mdl-list__item" ] [ text ("Name: " ++ vm.name) ]
+loading : Html Msg
+loading =
+    h1 [] [ text "Loading" ]
 
 
 
@@ -126,53 +97,39 @@ subscriptions model =
 -- HTTP
 
 
-fetchDeployments : Cmd Msg
-fetchDeployments =
-    let
-        url =
-            "http://localhost:8001/bosh/00000000-0000-0000-0000-000000000000/deployments"
-    in
-        Task.perform FetchFail FetchDeploymentsSucceed <| Http.get decodeDeployments url
-
-
-fetchVMs : List Deployment -> Cmd Msg
-fetchVMs deployemnts =
-    Task.perform FetchFail FetchVMsSucceed <| fetchVMsTasks deployemnts
-
-
-fetchVMsTasks : List String -> Task.Task Http.Error (List (List VM))
-fetchVMsTasks deployments =
-    Task.sequence
-        <| List.map fetchVMsTask deployments
-
-
-fetchVMsTask : String -> Task.Task Http.Error (List VM)
-fetchVMsTask deployment =
+getVMsTask : Deployment -> Cmd Msg
+getVMsTask deployment =
     let
         url =
             "http://localhost:8001/bosh/00000000-0000-0000-0000-000000000000/deployments/"
                 ++ deployment
                 ++ "/vms?format=full"
     in
-        Http.get decodeVMs url
+        Task.perform GetVMsTaskFail GetVMsTaskSucceed <| Http.get decodeVMsTask url
 
 
-decodeDeployments : Decoder (List Deployment)
-decodeDeployments =
-    Json.Decode.list decodeDeployment
+
+-- getTaskState : TaskUrl -> Cmd Msg
+-- getTaskResult : TaskUrl -> Cmd Msg
+-- DECODE
 
 
-decodeDeployment : Decoder Deployment
-decodeDeployment =
-    "name" := string
+decodeVMsTask : Decoder TaskUrl
+decodeVMsTask =
+    "location" := string
 
 
-decodeVMs : Decoder (List VM)
-decodeVMs =
-    Json.Decode.list decodeVM
 
-
-decodeVM : Decoder VM
-decodeVM =
-    object1 VM
-        ("name" := string)
+-- decodeDeployments : Decoder (List Deployment)
+-- decodeDeployments =
+--     Json.Decode.list decodeDeployment
+-- decodeDeployment : Decoder Deployment
+-- decodeDeployment =
+--     "location" := string
+-- decodeVMs : Decoder (List VM)
+-- decodeVMs =
+--     Json.Decode.list decodeVM
+-- decodeVM : Decoder VM
+-- decodeVM =
+--     object1 VM
+--         ("name" := string)
