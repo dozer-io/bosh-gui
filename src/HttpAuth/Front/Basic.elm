4df74aa2cee5e59147d1e4cf969fed5c660f698a
Ruben Koster <rkoster@starkandwayne.com>
2016-10-29 22:46:36 +0200

    Style HttpBasic Login From

--- src/HttpAuth/Front/Basic.elm
+++ src/HttpAuth/Front/Basic.elm
@@ -1,10 +1,14 @@
 module HttpAuth.Front.Basic exposing (view, update, Msg)
 
 import Material
-import Material.Grid exposing (grid, cell, size, Device(..))
+import Material.Grid exposing (grid, noSpacing, cell, size, offset, stretch, align, Align(..), Device(..))
 import Material.Textfield as Textfield
 import Material.Button as Button
-import Html as Html
+import Material.Options exposing (css)
+import Material.Card as Card
+import Material.Color as Color
+import Material.Typography exposing (right)
+import Html as Html exposing (text, p)
 import HttpAuth
 import HttpAuth.Basic exposing (HttpBasicClient, clientRequiresInput)
 
@@ -54,34 +58,57 @@ view client =
     in
         if client.requiresInput then
             Just
-                <| grid []
-                    [ cell [ size All 12 ]
-                        [ Textfield.render Mdl
-                            [ 0 ]
-                            client.mdl
-                            [ Textfield.label "Username"
-                            , Textfield.floatingLabel
-                            , Textfield.value username
-                            , Textfield.onInput ChangeUsername
+                <| grid
+                    [ noSpacing
+                    , css "position" "absolute"
+                    , css "width" "100%"
+                    , css "top" "0"
+                    , css "bottom" "0"
+                    , css "padding-bottom" "20%"
+                    , Color.background Color.primary
+                    ]
+                    [ cell [ align Middle, size All 2, offset All 5 ]
+                        [ Card.view []
+                            [ Card.title []
+                                [ Card.head [] [ text "Please Login" ] ]
+                            , Card.text []
+                                [ Textfield.render Mdl
+                                    [ 0 ]
+                                    client.mdl
+                                    [ Textfield.label "Target"
+                                    , Textfield.floatingLabel
+                                    , Textfield.disabled
+                                    , Textfield.value client.endpoint
+                                    , Textfield.onInput ChangeUsername
+                                    ]
+                                , Textfield.render Mdl
+                                    [ 1 ]
+                                    client.mdl
+                                    [ Textfield.label "Username"
+                                    , Textfield.floatingLabel
+                                    , Textfield.value username
+                                    , Textfield.onInput ChangeUsername
+                                    ]
+                                , Textfield.render Mdl
+                                    [ 2 ]
+                                    client.mdl
+                                    [ Textfield.label "Password"
+                                    , Textfield.floatingLabel
+                                    , Textfield.value password
+                                    , Textfield.onInput ChangePassword
+                                    ]
+                                ]
+                            , Card.actions [ Card.border, right ]
+                                [ Button.render Mdl
+                                    [ 0 ]
+                                    client.mdl
+                                    [ Button.primary
+                                    , Button.ripple
+                                    , Button.onClick Submit
+                                    ]
+                                    [ Html.text "Login" ]
+                                ]
                             ]
-                        , Textfield.render Mdl
-                            [ 1 ]
-                            client.mdl
-                            [ Textfield.label "Password"
-                            , Textfield.floatingLabel
-                            , Textfield.value password
-                            , Textfield.onInput ChangePassword
-                            ]
-                        ]
-                    , cell [ size All 12 ]
-                        [ Button.render Mdl
-                            [ 0 ]
-                            client.mdl
-                            [ Button.raised
-                            , Button.ripple
-                            , Button.onClick Submit
-                            ]
-                            [ Html.text "Login" ]
                         ]
                     ]
         else
