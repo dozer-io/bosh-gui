ce4f5d8b423c5205b57d5748b858bd3083d6b907
Ruben Koster <rkoster@starkandwayne.com>
2016-10-29 11:26:19 +0200

    Remove dozer references + added License

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 if (process.env.NODE_ENV != 'production') {
   require("!style!css!material-design-icons/iconfont/material-icons.css");
-
-  require("!style!css!material-design-lite/dist/material.blue-amber.min.css");
+  require("!style!css!material-design-lite/dist/material.blue_grey-amber.min.css");
 }
 
 // inject bundled Elm app into div#main
-var Elm = require( './Dozer' );
+var Elm = require( './Bosh' );
 
-if (process.env.NODE_ENV == 'production') {
-  Elm.Dozer.fullscreen({
-    apiUrl : "https://api.dozer.io",
-    authUrl : "https://login.dozer.io/oauth/authorize",
-    appUrl : "https://beta-console.dozer.io",
-    oAuthClient : "dozer-web-beta",
-    oAuthScopes : [ "dozer_api.user", "bosh_api.user", "bosh.*.admin" ]
-  });
-} else {
-  Elm.Dozer.fullscreen({
-    apiUrl : "http://localhost:8001/dozer",
-    authUrl : "http://localhost:8001/login/authorize",
-    appUrl : "http://localhost:8080",
-    oAuthClient : "dozer-web-beta",
-    oAuthScopes : [ "dozer_api.user", "bosh_api.user", "bosh.*.admin" ]
-  });
-}
+Elm.Bosh.fullscreen();
